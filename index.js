72164e0862b4333e17d7fe6e8500e7549704dd0f
Ray Deck <ray@raydeck.com>
2020-04-26 10:15:18 -0500

    Updating the API generator to clean up after itself.

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 #!/usr/bin/env node
 const mustache = require("mustache");
 const { join } = require("path");
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync, mkdirSync, existsSync } = require("fs");
 const { codegen } = require(join(__dirname, "package.json"));
 const { spawnSync } = require("child_process");
 const tmp = require("tmp");
-const getFastlaneAPI = () => {
+const { format } = require("prettier");
+const { sync: rimraf } = require("rimraf");
+const getFastlaneAPI = ({ debug }) => {
   const template = readFileSync(
     join(__dirname, "templates", "Fastfile.mustache"),
     { encoding: "utf8" }
   );
-  const target = join(__dirname, "fastlane", "Fastfile");
+  const fastlanepath = join(__dirname, "fastlane");
+  if (!existsSync(fastlanepath)) mkdirSync(fastlanepath);
+  const target = join(fastlanepath, "Fastfile");
   const { name: tempFile, removeCallback, ...stuff } = tmp.fileSync();
-  console.log(tempFile);
-  const out = mustache.render(template, { tempFile, ...codegen });
+  const out = format(mustache.render(template, { tempFile, ...codegen }), {
+    parser: "ruby",
+  });
   writeFileSync(target, out);
   spawnSync("bundle", ["exec", "fastlane", "codegen"]);
-  console.log("Looking at the file", tempFile);
   const o = JSON.parse(readFileSync(tempFile, { encoding: "utf8" }));
-  removeCallback();
+  if (!debug) {
+    removeCallback();
+    rimraf(fastlanepath);
+  }
   return o;
 };
 module.exports = getFastlaneAPI;
